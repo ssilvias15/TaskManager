4a5b8d5c08802ecc828c662c71db53086fb0e609
Silvia <ssilvias15@yahoo.com>
2016-07-10 10:20:21 +0100

    added resources

--- src/main/java/ie/silvia/spring/config/SecurityConfig.java
+++ src/main/java/ie/silvia/spring/config/SecurityConfig.java
@@ -61,8 +61,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	protected void configure(HttpSecurity http) throws Exception {
 
-
-
+    	
+    	http.authorizeRequests().antMatchers("/resources/**").permitAll();
     	//
     	
     	http.csrf().disable();
