880629b32fa96b1d23e12275cbd360e07078cb4a
Silvia <ssilvias15@yahoo.com>
2016-07-20 11:53:00 +0100

    my task associated with current user

--- WebContent/WEB-INF/jsp/mytasks.jsp
+++ WebContent/WEB-INF/jsp/mytasks.jsp
@@ -1,12 +1,21 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+	pageEncoding="ISO-8859-1"%>
+<%@ page import="org.springframework.security.core.userdetails.User"%>
+<%@ page
+	import="org.springframework.security.core.context.SecurityContextHolder"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+<title>My tasks</title>
 </head>
 <body>
-<h2>PIZZA</h2>
+	<h2>My current tasks</h2>
+	<%
+		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String name = user.getUsername();
+		
+		out.println("LOGGED IN AS: " + name);
+	%>
 </body>
 </html>
--- src/main/java/ie/silvia/dao/impl/DAOUsers.java
+++ src/main/java/ie/silvia/dao/impl/DAOUsers.java
@@ -1,13 +1,35 @@
 package ie.silvia.dao.impl;
 
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+
 import ie.silvia.dao.GenericDaoHibernateImpl;
+import ie.silvia.hibernate.util.HibernateUtil;
 import ie.silvia.model.Users;
+import jdk.management.resource.internal.inst.SocketOutputStreamRMHooks;
 
 public class DAOUsers extends GenericDaoHibernateImpl<Users, Integer>{
 
 	public DAOUsers() {
 		super(Users.class);
 	}
+	
+	public Users getUserByUsername(String username){
+		Users user = null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Criteria criteria = session.createCriteria(Users.class);
 
+		user = (Users) criteria.add(Restrictions.eq("username", username)).uniqueResult();
+
+		return user;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("USER: ");
+		DAOUsers dao = new DAOUsers();
+		Users user = dao.getUserByUsername("silvia");
+		System.out.println("USER: " + user.getUsername()  + " " + user.getPassword());
+	}
 }
 
